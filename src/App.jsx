54e7d58dfdbbfc34bd11f291cd216603e28fc2a5
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-05-28 17:33:14 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Container from './components/Container';
+import { ContainerClassy, ContainerFunctional } from './components/Container';
 import './App.less';
 
 
-ReactDOM.render(<Container />, document.querySelector('#target1'));
+ReactDOM.render(
+  <>
+    <ContainerClassy />
+    <ContainerFunctional />
+  </>,
+  document.querySelector('#target1')
+);
--- src/components/Count.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// Implement Count component so it renders the `count` in an h3,
-// and also two button elements that invoke `increment` and `decrement` on click.
-// Wrap everything in a div with the class of 'sub-container' so styles will work.
-export default function Count({ count, increment, decrement }) {
-  return null;
-}
