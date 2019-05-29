7c9da69f684a4adbe2335e1309b91c10e0bd9b78
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-05-29 08:30:48 +0200

    wip

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -7,78 +7,16 @@ const initialFriendsList = [
   { id: '21973c2d-e74d-4ef9-bccb-d3af78a7f0f0', name: 'Sorin', fav: false },
 ];
 
-function Friend({ info, fav }) {
-  return (
-    <div>
-      {info.name}
-      {info.fav && <span>&#x2661;</span>}
-      <button onClick={() => fav(info.id)}>fav</button>
-    </div>
-  );
-}
-
 export class ContainerClassy extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      friends: initialFriendsList,
-      inputValue: '',
-    };
-  }
-
-  fav = (id) => {
-    this.setState(st => {
-      const updatedFriends = st.friends.map(friend => {
-        if (friend.id === id) {
-          friend.fav = !friend.fav;
-        }
-        return friend;
-      });
-      return { friends: updatedFriends };
-    });
-  }
-
-  onChange = evt => this.setState({ inputValue: evt.target.value })
-
-  addFriend = () => {
-    const newFriend = {
-      name: this.state.inputValue,
-      id: uuid(),
-      fav: false,
-    };
-    this.setState({
-      friends: this.state.friends.concat(newFriend),
-      inputValue: '',
-    });
-  }
-
   render() {
     return (
-      <div className='container'>
-        {
-          this.state.friends.map(fr => (
-            <Friend key={fr.id} info={fr} fav={this.fav} />
-          ))
-        }
-        <AddFriend
-          name={this.state.inputValue}
-          onChange={this.onChange}
-          addFriend={this.addFriend}
-        />
-      </div>
+      <div>I am classy</div>
     );
   }
 }
 
-function AddFriend({ name, onChange, addFriend }) {
-  return (
-    <div>
-      <input value={name} onChange={onChange} type="text" />
-      <button disabled={!name} onClick={addFriend}>Add Friend</button>
-    </div>
-  );
-}
-
 export function ContainerFunctional() {
-  return <div></div>;
+  return (
+    <div>I am functional</div>
+  );
 }
